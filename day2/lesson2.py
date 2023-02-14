63f14b03d58245573551172febf665196130c8f2
SpagHeddyy <birjamov@bk.ru>
2023-02-14 04:38:22 +0400

    next commit

---
+++ day2/lesson2.py
@@ -0,0 +1,11 @@
+name =  "chemi saxeli giorgi"
+surname = "gvari bijamovi"
+to_me = "me var"
+age = 24
+years = "wlis"
+my_height = "chemi simagle"
+height = 175.5
+print(name + " " + surname + " " + to_me + " " + str(age) + " " + years + " " + my_height + " " + str(height) )
+
+
+
