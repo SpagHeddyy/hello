59a816aad317809e58d5cec41a3b6e80bdcf14c8
SpagHeddyy <birjamov@bk.ru>
2023-02-17 16:51:36 +0400

    day 2 commit

---
+++ day 2/lesson3.py
@@ -0,0 +1,25 @@
+name = "giorgi"
+print(len(name))
+if 6<9:
+    print("done")
+full_name =  "giorgi bidjamovi"
+print(len(full_name))
+print(full_name[0])
+print(full_name[10])
+print("k" in full_name)
+print("v" in full_name)
+print("r" not in full_name)
+print("s" not in full_name)
+print(full_name[0:3])
+print(full_name[3:6])
+print(full_name[0:16:2])
+print(full_name[:6])
+print(full_name[6:])
+print(full_name[-12] + "oal")
+print(full_name[-1:-5:-1])
+print(full_name[-1:-5:-2])
+print(full_name.upper())
+name2="GIORGI"
+print(name2.lower())
+print(full_name.replace("i", "#" ))
+
